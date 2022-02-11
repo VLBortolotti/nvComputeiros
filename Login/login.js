34d072143665c26edc4e53e3d2fbc4b6737f996c
VLBortolotti <vitorlacobortolotti@gmail.com>
2022-02-11 20:32:52 -0300

    Davi Commit

--- Login/login.js
+++ Login/login.js
@@ -12,9 +12,36 @@ function trocarForm(className, e) {
 }
 
 // Validando se as senhas no registro sao iguais
+
 const senhaRegistro = document.querySelector('form.register #password');
 const senhaRegistroConfirmacao = document.querySelector('form.register #confirm-pass');
 
 senhaRegistro.addEventListener('input', function () {
 	senhaRegistroConfirmacao.pattern = `${this.value}`;
 })
+
+
+if (".password" < 8){
+	function cadastrar(){
+		const cadastro = {
+
+	
+			email: document.querySelector('.email').value, senha: document.querySelector('.password').value, 
+			
+		};
+		
+
+
+		localStorage.setItem('cadastro', JSON.stringify(cadastro));
+
+		const cadass = JSON.parse(localStorage.getItem('cada'));
+		console.log(cadass.email)
+	}
+	let cadastro = JSON.parse(localStorage.getItem('cada'));
+		
+	
+}
+
+
+
+
